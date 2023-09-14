fb022dab6167fe83cbda6102d9c087e95780ff8e
JOAQUIN BEJAR <jb@taunais.com>
2023-09-14 19:51:17 +0100

    Refactor LoggerFactory to initialize NO_LOGGER within initLoggersIfNeeded
    Removed the problematic static initialization of NO_LOGGER that caused an uninitialized variable warning. Moved the initialization logic to the initLoggersIfNeeded method to ensure NO_LOGGER is properly initialized before use.

--- appveyor-download.bat
+++ appveyor-download.bat
@@ -2,10 +2,12 @@
 set archive=http://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-%DB%/winx64-packages/mariadb-%DB%-winx64.msi
 set last=http://mirror.i3d.net/pub/mariadb/mariadb-%DB%/winx64-packages/mariadb-%DB%-winx64.msi
 
+echo Downloading from %archive%
 curl -fLsS -o server.msi %archive%
 
 if %ERRORLEVEL% == 0 goto end
 
+echo Downloading from %last%
 curl -fLsS -o server.msi %last%
 if %ERRORLEVEL% == 0  goto end
 
